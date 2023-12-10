367ccab663fdc369fb49fccb4dbc5036394c95d5
Richard Lee <rlee@tokyo3.com>
2023-12-10 07:17:47 -0800

    improvements to front end ui

--- docker/llm-service/static/script.js
+++ docker/llm-service/static/script.js
@@ -1,10 +1,44 @@
-function generateText() {
-    const inputPrompt = document.getElementById('inputPrompt').value;
-    // Use window.location.origin to get the base URL dynamically
+let conversationHistory = [];
+
+function updateConversation(msg, isUser) {
+    let formattedMsg = (isUser ? "User: " : "Bot: ") + msg;
+    conversationHistory.push(formattedMsg);
+
+    // Keep only the last N interactions, if necessary
+    const maxHistorySize = 10;  // Example size
+    if (conversationHistory.length > maxHistorySize) {
+        conversationHistory.shift(); // Remove the oldest interaction
+    }
+
+    // Update textarea content
+    document.getElementById("conversation").value = conversationHistory.join("\n");
+}
+
+function sendMessage() {
+    let userInput = document.getElementById("userInput").value;
+    if (userInput.trim() === "") return;
+
+    updateConversation(userInput, true);
+
+    // Call the backend API with the full conversation history
+    const fullPrompt = conversationHistory.join("\n") + "\nBot: ";
     const baseUrl = window.location.origin;
-    fetch(`${baseUrl}/generate/?prompt=${encodeURIComponent(inputPrompt)}`)
+    fetch(`${baseUrl}/generate/?prompt=${encodeURIComponent(fullPrompt)}`)
         .then(response => response.json())
-        .then(data => document.getElementById('outputText').innerText = data.generated_text)
+        .then(data => {
+            updateConversation(data.generated_text, false);
+        })
         .catch(error => console.error('Error:', error));
+
+    // Clear input field
+    document.getElementById("userInput").value = "";
+}
+
+window.onload = () => {
+    document.getElementById("userInput").addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            sendMessage();
+        }
+    });
 }
 
