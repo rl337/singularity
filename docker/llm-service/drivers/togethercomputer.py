dae56d5ae01c4765abd1b4661a4d74ccc78bfc5f
Richard Lee <rlee@tokyo3.com>
2023-12-30 08:07:09 -0800

    updated base image to include new requirements. added a couple new models to config

--- docker/llm-service/drivers/togethercomputer.py
+++ docker/llm-service/drivers/togethercomputer.py
@@ -1,8 +1,19 @@
 import torch
 import logging
-from drivers.common import TextGeneratorRequest, TextGeneratorDriver
-from transformers import AutoTokenizer, AutoModelForCausalLM, PreTrainedTokenizer, PreTrainedModel
+import sys
 
+from drivers.common import TextGeneratorRequest, TextGeneratorDriver
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, PreTrainedTokenizer, PreTrainedModel, PretrainedConfig
+from torch.quantization import quantize_dynamic
+
+
+
+class Llama2TextGeneratorRequest(TextGeneratorRequest):
+    max_new_tokens: int
+    do_sample: bool
+    temperature: float
+    top_p: float
+    top_k: int
 
 # Driver for togethercomputer/Llama-2-7B-32K-Instruct
 class Llama2TextGeneratorDriver(TextGeneratorDriver):
@@ -55,6 +66,61 @@ class RedPajamaTextGeneratorDriver(TextGeneratorDriver):
         # outputs = self.model.generate(
         #     **inputs, max_new_tokens=128, do_sample=True, temperature=0.7, top_p=0.7, top_k=50, return_dict_in_generate=True
         # )
+        token = outputs.sequences[0, input_length:]
+        output_text = self.tokenizer.decode(token)
+        return {"generated_text": output_text}
+
+
+class StripedHyenaTextGeneratorRequest(TextGeneratorRequest):
+    max_new_tokens: int
+    temperature: float
+    top_p: float
+    top_k: int
+    repetition_penalty: float
+    penalty_alpha: float
+    do_sample: bool
+    eos_token_id: int 
+
+# Driver for StripedHyena-Nous-7B 
+class StripedHyenaTextGeneratorDriver(TextGeneratorDriver):
+    tokenizer: PreTrainedTokenizer
+    model: PreTrainedModel
+    config: PretrainedConfig
+
+    def initialize_generator(self):
+        logging.info(f"initializing from {self.model_path}")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_path,
+            model_max_length=sys.maxsize,
+            trust_remote_code=True,
+            local_files_only=True
+        )
+        self.config = AutoConfig.from_pretrained(
+            self.model_path,
+            trust_remote_code=True,
+            local_files_only=True
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            self.model_path,
+            trust_remote_code=True,
+            config=self.config,
+            local_files_only=True
+        )
+
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        device = "cpu"
+        self.model.to(device)
+
+    def generate_text(self, request: StripedHyenaTextGeneratorRequest):
+        input_ids = self.tokenizer.encode(request.prompt, return_tensors="pt")
+
+        kwargs = request.dict()
+        kwargs.pop("prompt", None)
+        logging.info(f"request was: {kwargs}")
+
+        inputs = self.tokenizer(request.prompt, return_tensors='pt').to(self.model.device)
+        input_length = inputs.input_ids.shape[1]
+        outputs = self.model.generate(**kwargs, **inputs)
         token = outputs.sequences[0, input_length:]
         output_text = self.tokenizer.decode(token)
         return {"generated_text": output_text}
