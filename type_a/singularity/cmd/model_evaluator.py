21cd3b91330f21cd010dce73a49c3c11804e9dc7
Richard Lee <rlee@tokyo3.com>
2024-03-11 08:58:28 -0700

    added a simple model evaluator script.

--- type_a/singularity/cmd/model_evaluator.py
+++ type_a/singularity/cmd/model_evaluator.py
@@ -10,33 +10,30 @@ from datasets import load_dataset
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 import torch.nn as nn
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # Create the parser
-parser = argparse.ArgumentParser(description="Train a model on a specified dataset.")
+parser = argparse.ArgumentParser(description="evaluate a trained model on a specified dataset.")
 
 # Add arguments
 parser.add_argument("base_model_dir", type=str, help="Directory containing the base model.")
 parser.add_argument("dataset", type=str, help="path to or dataset from huggingface")
-parser.add_argument("output_dir", type=str, help="Directory where the fine-tuned model will be written.")
-#parser.add_argument("--validation_set", type=str, default=None, help="Optional tarball (.tgz) file containing the validation set.")
-#parser.add_argument("--max_length", type=int, default=1024, help="max_length for the tokenizer")
 parser.add_argument("--epochs", type=int, default=32, help="block size for the tokenizer")
 parser.add_argument("--batch_size", type=int, default=8, help="data loader batch size")
 
 # Parse arguments
 args = parser.parse_args()
-# model_id = "rl337/cicero-ffn"
 conf = AutoConfig.from_pretrained(args.base_model_dir, local_files_only=True, trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained(args.base_model_dir, config=conf, local_files_only=True, trust_remote_code=True)
 dataset = load_dataset(args.dataset, trust_remote_code=True)
 
 def concatenate_texts(dataset_dict):
     concatenated_text = ''
-    for i in range(1, 5):  # Assuming books are named 'book1' to 'book4'
+    for i in [5]:  
         for line in dataset_dict[f'book{i}']['text']:
             book_text = ''.join([char if ord(char) < 128 else ' ' for char in line])
             concatenated_text += f'\r{book_text}'
@@ -67,43 +64,46 @@ training_text = concatenate_texts(dataset)
 # Generate sequences from the concatenated training text
 sequences, labels = generate_sequences(training_text)
 sequences_one_hot = [one_hot_encode(x) for x in sequences]
-sequences_tensor = torch.stack([torch.tensor(seq, dtype=torch.float32) for seq in sequences_one_hot])
+sequences_tensor = torch.stack([seq.clone().detach().to(dtype=torch.float32) for seq in sequences_one_hot])
 labels_tensor = torch.tensor(labels, dtype=torch.long)
 
 
 dataset = TensorDataset(sequences_tensor, labels_tensor)
 data_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
 
+model = None
 try:
     model = AutoModel.from_pretrained(args.base_model_dir, config=conf, local_files_only=True, trust_remote_code=True)
-    logger.info(f"Loaded pre-trained model from {args.base_model_dir}")
 except (OSError, ValueError) as e:
-    logger.info(f"No pretrained weights found creating a fresh model from {args.base_model_dir}")
-    model = AutoModel.from_config(conf, name_or_path=args.base_model_dir, local_files_only=True, trust_remote_code=True)
+    logger.fatal("Couldn't load pretrained model", exc_info=1)
+    sys.exit(-1)
 
-
-optimizer = AdamW(model.parameters(), lr=5e-5)  # Set up the optimizer
 total_steps = len(data_loader) * args.epochs  # Total number of training steps
 
-loss_function = nn.CrossEntropyLoss()
+# Placeholders for predictions and true labels
+all_preds = []
+all_true_labels = []
 
-# Training loop
-model.train()
-for epoch in range(args.epochs):
-    for batch in data_loader:
-        batch_input, batch_labels = batch
-        # inputs = inputs.to('cuda')
-        # masks = masks.to('cuda')
-        outputs = model(batch_input)
-        loss = loss_function(outputs, batch_labels)  # Adjust this line as per your model's output and task
+model.eval()
+for batch in data_loader:
+    inputs, labels = batch
+    with torch.no_grad():  # Do not compute gradients during evaluation
+        outputs = model(inputs)
+        preds = torch.argmax(outputs, dim=-1)  # Assuming classification task
+    
+    all_preds.extend(preds.cpu().numpy())
+    all_true_labels.extend(labels.cpu().numpy())
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    logger.info(f"completed epoch {epoch} of {args.epochs}")
+# Calculate metrics
+accuracy = accuracy_score(all_true_labels, all_preds)
+# Calculate macro-averaged precision and recall
+precision_macro, recall_macro, f1_macro, _ = precision_recall_fscore_support(all_true_labels, all_preds, average='macro', zero_division=0)
 
+# Calculate micro-averaged precision and recall
+precision_micro, recall_micro, f1_micro, _ = precision_recall_fscore_support(all_true_labels, all_preds, average='micro', zero_division=0)
 
-# Save the fine-tuned model
-model.save_pretrained(args.output_dir)
-tokenizer.save_pretrained(args.output_dir)
+print(f"Macro-averaged Precision: {precision_macro}, Recall: {recall_macro}")
+print(f"Micro-averaged Precision: {precision_micro}, Recall: {recall_micro}")
+
+print(f"Accuracy: {accuracy}")
 
